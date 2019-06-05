4dfaf5dffc4d9760cdb5592365baf008bca42913
chetan22 <ck.chetan20@gmail.com>
2019-06-05 19:46:17 +0530

    Add other activations and GPU

--- feedforward-neural-network/nn.py
+++ feedforward-neural-network/nn.py
@@ -28,27 +28,41 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dset,
 # Defining model class
 class feed_forward_neural_network(nn.Module):
 
-	def __init__(self, input_dim, hidden_dim, output_dim):
+	def __init__(self, input_dim, hidden_dim,
+				 output_dim, activation_func):
+
 		super().__init__()
 		# initializin forward-cost-1
 		self.fc1 = nn.Linear(input_dim, hidden_dim)
 		# initializing sigmoid activation
-		self.sigmoid = nn.Sigmoid()
+		if activation_func == 1:
+			self.activation_func = nn.Sigmoid()
+		elif activation_func == 2:
+			self.activation_func = nn.Tanh()
+		else:
+			self.activation_func = nn.ReLU()
 		# initializing forward-cost-2
 		self.fc2 = nn.Linear(hidden_dim, output_dim)
 
 	def forward(self, X):
 		z1 = self.fc1(X)
-		z2 = self.sigmoid(z1)
+		z2 = self.activation_func(z1)
 		z3 = self.fc2(z2)
 		return z3
 
 
+# Input activation function:
+activation_func = input('1. sigmoid\n2. Tanh\n3. ReLU\n')
 # Instantiate model
 input_dim = 28*28
 hidden_dim = 100
 output_dim = 10
-model = feed_forward_neural_network(input_dim, hidden_dim, output_dim)
+model = feed_forward_neural_network(input_dim, hidden_dim,
+									output_dim, activation_func)
+# Checking for gpu
+device = torch.device('cuda:0' if torch.cuda.is_available()
+					  else 'cpu')
+model.to(device)
 # Instantiate loss class
 loss_func = nn.CrossEntropyLoss()
 # Instantiate optimizer class
@@ -59,8 +73,8 @@ iter_ = 0
 for epoch in range(num_epochs):
 	for images, labels in train_loader:
 		# Load images
-		images = Variable(images.view(-1, 28*28))
-		labels = Variable(labels)
+		images = Variable(images.view(-1, 28*28).to(device))
+		labels = Variable(labels.to(device))
 		# Clear out accumulated grads
 		optimizer.zero_grad()
 		# Feed forward
@@ -78,12 +92,11 @@ for epoch in range(num_epochs):
 			total, correct = 0, 0
 			for images, labels in test_loader:
 				# Load images
-				images = Variable(images.view(-1, 28*28))
+				images = Variable(images.view(-1, 28*28).to(device))
 				# predict from current model
 				labels_predit = model(images)
 				predicted_val, predicted_labels= torch.max(labels_predit.data, 1)
 				total += labels.size(0)
-				correct += (labels == predicted_labels).sum()
+				correct += (labels.cpu() == predicted_labels.cpu()).sum()
 			accuracy = 100 * correct / total
-			print('iter:', iter_, 'accuracy:', accuracy, 'epoch:', epoch)
-
+			print('iter:', iter_, 'accuracy:', accuracy)
