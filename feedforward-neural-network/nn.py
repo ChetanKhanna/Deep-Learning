aef8caa0bfe0c91ce06b647af43190e5c91be96f
chetan22 <ck.chetan20@gmail.com>
2019-08-06 15:54:05 +0530

    Add neural net

--- feedforward-neural-network/nn.py
+++ feedforward-neural-network/nn.py
@@ -8,47 +8,49 @@ import torchvision.datasets as dsets
 
 # Getting dataset
 train_dset = dsets.MNIST(root='./data',
-						 train=True,
-						 transform=transforms.ToTensor(),
-						 download=True)
+                         train=True,
+                         transform=transforms.ToTensor(),
+                         download=True)
 test_dset = dsets.MNIST(root='./data',
-						train=False,
-						transform=transforms.ToTensor(),
-						download=True)
+                        train=False,
+                        transform=transforms.ToTensor(),
+                        download=True)
 
 # Make data interable
 batch_size = 100
 num_epochs = 5
 train_loader = torch.utils.data.DataLoader(dataset=train_dset,
-										   batch_size=batch_size,
-										   shuffle=True)
+                                           batch_size=batch_size,
+                                           shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_dset,
-										  batch_size=batch_size,
-										  shuffle=False)
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+
 # Defining model class
 class feed_forward_neural_network(nn.Module):
 
-	def __init__(self, input_dim, hidden_dim,
-				 output_dim, activation_func):
+    def __init__(self, input_dim, hidden_dim,
+                 output_dim, activation_func):
 
-		super().__init__()
-		# initializin forward-cost-1
-		self.fc1 = nn.Linear(input_dim, hidden_dim)
-		# initializing sigmoid activation
-		if activation_func == 1:
-			self.activation_func = nn.Sigmoid()
-		elif activation_func == 2:
-			self.activation_func = nn.Tanh()
-		else:
-			self.activation_func = nn.ReLU()
-		# initializing forward-cost-2
-		self.fc2 = nn.Linear(hidden_dim, output_dim)
+        super().__init__()
+        # initializin forward-cost-1
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        # initializing sigmoid activation
+        if activation_func == 1:
+            self.activation_func = nn.Sigmoid()
+        elif activation_func == 2:
+            self.activation_func = nn.Tanh()
+        else:
+            self.activation_func = nn.ReLU()
+        # initializing forward-cost-2
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
 
-	def forward(self, X):
-		z1 = self.fc1(X)
-		z2 = self.activation_func(z1)
-		z3 = self.fc2(z2)
-		return z3
+    def forward(self, X):
+        z1 = self.fc1(X)
+        z2 = self.activation_func(z1)
+        z3 = self.fc2(z2)
+        return z3
 
 
 # Input activation function:
@@ -58,10 +60,10 @@ input_dim = 28*28
 hidden_dim = 100
 output_dim = 10
 model = feed_forward_neural_network(input_dim, hidden_dim,
-									output_dim, activation_func)
+                                    output_dim, activation_func)
 # Checking for gpu
 device = torch.device('cuda:0' if torch.cuda.is_available()
-					  else 'cpu')
+                      else 'cpu')
 model.to(device)
 # Instantiate loss class
 loss_func = nn.CrossEntropyLoss()
@@ -71,32 +73,32 @@ optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 # Train model
 iter_ = 0
 for epoch in range(num_epochs):
-	for images, labels in train_loader:
-		# Load images
-		images = Variable(images.view(-1, 28*28).to(device))
-		labels = Variable(labels.to(device))
-		# Clear out accumulated grads
-		optimizer.zero_grad()
-		# Feed forward
-		labels_predit = model(images)
-		# Get loss
-		loss = loss_func(labels_predit, labels)
-		# Get grads
-		loss.backward()
-		# Update params
-		optimizer.step()
+    for images, labels in train_loader:
+        # Load images
+        images = Variable(images.view(-1, 28*28).to(device))
+        labels = Variable(labels.to(device))
+        # Clear out accumulated grads
+        optimizer.zero_grad()
+        # Feed forward
+        labels_predit = model(images)
+        # Get loss
+        loss = loss_func(labels_predit, labels)
+        # Get grads
+        loss.backward()
+        # Update params
+        optimizer.step()
 
-		iter_ += 1
-		if iter_ % 500 == 0:
-			# Calculate accuracy
-			total, correct = 0, 0
-			for images, labels in test_loader:
-				# Load images
-				images = Variable(images.view(-1, 28*28).to(device))
-				# predict from current model
-				labels_predit = model(images)
-				predicted_val, predicted_labels= torch.max(labels_predit.data, 1)
-				total += labels.size(0)
-				correct += (labels.cpu() == predicted_labels.cpu()).sum()
-			accuracy = 100 * correct / total
-			print('iter:', iter_, 'accuracy:', accuracy)
+        iter_ += 1
+        if iter_ % 500 == 0:
+            # Calculate accuracy
+            total, correct = 0, 0
+            for images, labels in test_loader:
+                # Load images
+                images = Variable(images.view(-1, 28*28).to(device))
+                # predict from current model
+                labels_predit = model(images)
+                predicted_val, predicted_labels = torch.max(labels_predit.data, 1)
+                total += labels.size(0)
+                correct += (labels.cpu() == predicted_labels.cpu()).sum()
+            accuracy = 100 * correct / total
+            print('iter:', iter_, 'accuracy:', accuracy)
