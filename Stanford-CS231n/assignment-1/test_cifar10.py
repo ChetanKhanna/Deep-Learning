c7825fa3db301e8506993bab6502687a2ee00533
chetan22 <ck.chetan20@gmail.com>
2019-08-07 23:04:25 +0530

    Add script to run models on cifar10 dataset

---
+++ Stanford-CS231n/assignment-1/test_cifar10.py
@@ -0,0 +1,60 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import pickle
+from linear_classifier import LinearSVM
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+
+# dataset loading and required functions
+def unpickle(file):
+    '''Load byte data from file'''
+    with open(file, 'rb') as f:
+        data = pickle.load(f, encoding='latin-1')
+        return data
+
+def load_cifar10_data(data_dir):
+    '''Return train_data, train_labels, test_data, test_labels
+    The shape of data is 32 x 32 x3'''
+    train_data = None
+    train_labels = []
+    # unpickling train dataset
+    for i in range(1, 6):
+        data_dic = unpickle(data_dir + "/data_batch_{}".format(i))
+        if i == 1:
+            train_data = data_dic['data']
+        else:
+            train_data = np.vstack((train_data, data_dic['data']))
+        train_labels += data_dic['labels']
+    # unpickling test dataset
+    test_data_dic = unpickle(data_dir + "/test_batch")
+    test_data = test_data_dic['data']
+    test_labels = test_data_dic['labels']
+    # reshaping train dataset
+    # train_data = train_data.reshape((len(train_data), 3, 32, 32))
+    # train_data = np.rollaxis(train_data, 1, 4)
+    train_labels = np.array(train_labels)
+    # reshaping test dataset
+    # test_data = test_data.reshape((len(test_data), 3, 32, 32))
+    # test_data = np.rollaxis(test_data, 1, 4)
+    test_labels = np.array(test_labels)
+    # return train and test dataset and labels
+    return train_data, train_labels, test_data, test_labels
+
+data_dir = os.path.join(os.path.expanduser('~'), 'Deep-Learning',
+                        'data', 'cifar-10-batches-py')
+train_data, train_labels, test_data, test_labels = load_cifar10_data(data_dir)
+# printing dataset shape
+print(train_data.shape)
+print(train_labels.shape)
+print(test_data.shape)
+print(test_labels.shape)
+# model training
+model = LinearSVM()
+model.train(train_data, train_labels, verbose=True)
+pred = model.predict(test_data)
+print(accuracy_score(test_labels, pred))
+# model = LinearSVC()
+# model.fit(train_data, train_labels)
+# pred = model.predict(test_data)
+# print(accuracy_score(test_labels, pred))
