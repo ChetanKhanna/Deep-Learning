0c4db2253344eb8cc64d252649781a92d324170f
chetan22 <ck.chetan20@gmail.com>
2019-08-23 15:05:05 +0530

    Replace HtmlResponse with TemplateResponse and add CSS to html files

--- fast.ai_course/fruits/app.py
+++ fast.ai_course/fruits/app.py
@@ -1,5 +1,6 @@
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse, HTMLResponse, RedirectResponse
+from starlette.responses import RedirectResponse
+from starlette.templating import Jinja2Templates
 from fastai.vision import open_image, load_learner
 from pathlib import Path
 from io import BytesIO
@@ -16,6 +17,8 @@ classes = ['green-apple', 'guava', 'pear']
 path = Path(os.getcwd())
 model = load_learner(path) # load the pre-trained model
 
+templates = Jinja2Templates(directory='data/Templates')
+
 # function to return image(in bytes) from given url
 async def get_bytes_from_url(url):
     async with aiohttp.ClientSession() as session:
@@ -23,47 +26,32 @@ async def get_bytes_from_url(url):
             return await response.read()
 
 # function to classify image from image passes as bytes
-def predict_image_from_bytes(bytes_):
+def predict_image_from_bytes(request, bytes_):
     img = open_image(BytesIO(bytes_))
     _, _, output = model.predict(img)
-    return JSONResponse({
-        "predictions": sorted(
-            zip(classes, map(float, output)),
-            key=lambda p: p[1],
-            reverse=True)
-        })
+    prediction = sorted(zip(classes, map(float, output)),
+                        key=lambda p: p[1], reverse=True)
+    return templates.TemplateResponse('result.html',
+                                     {'request': request,
+                                      'prediction': prediction})
 
 # upload-image function
 @app.route('/upload', methods=["POST"])
 async def upload(request):
     data = await request.form()
     bytes_ = await (data['file'].read())
-    return predict_image_from_bytes(bytes_)
+    return predict_image_from_bytes(request, bytes_)
 
 # classify-from-url functino
 @app.route('/classify-url', methods=["GET"])
 async def classify_url(request):
     bytes_ = await get_bytes_from_url(request.querry_params['url'])
-    return predict_image_from_bytes(bytes_)
+    return predict_image_from_bytes(request, bytes_)
 
 # defining home page for the app
 @app.route('/')
 def home(request):
-    return HTMLResponse("""
-        <h3>This app classifies fruits into Green-Apple, Guava or Pear</h3>
-
-        <form action="/upload" method="post" enctype="multipart/form-data">
-            Select image to upload:
-            <input type="file" name="file">
-            <input type="submit" value="Upload Image">
-        </form>
-
-        <form action=/classify-url" method="get">
-            Or submit a URL:
-            <input type="url" name="url">
-            <input type="submit" value="Fetch Image">
-        </form>
-        """)
+    return templates.TemplateResponse('index.html', {'request': request})
 
 # redirectino function
 @app.route('/form')
